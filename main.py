e6c3c8641992c6f064ff714f23b95705236f912b
Noelle <nastasavilova295@gmail.com>
2026-03-17 16:03:31 +0800

    data reshape

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ data_test_norm = data_test.astype('float32') / 255.0
 print(f'Before norm: [{data_train.min()} - {data_train.max()}]\n'
       f'After norm: [{data_train_norm.min():.2f} - {data_train_norm.max():.2f}]\n')
 
+# Reshape формы для Dense
+data_train_flat = data_train_norm.reshape(-1, 28 * 28)
+data_test_flat = data_test_norm.reshape(-1, 28 * 28)
+
+print(f'Before reshape:\n'
+      f'data_train_norm: {data_train_norm.shape}\n'
+      f'Single digit: {data_train_norm[0].shape}\n')
+
+print(f'After reshape:\n'
+      f'data_train_flat: {data_train_flat.shape}\n'
+      f'Single digit: {data_train_flat[0].shape}\n')
+
+print(f'Check: {28} * {28} = {28 * 28}')
